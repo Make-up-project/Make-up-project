7e56f4c1d70efa520979826fe22c711c1f554ecd
Ali-alterawi <alielterawe@gmail.com>
2023-05-05 22:23:10 +0300

    first-upload

---
+++ beauty-bloom/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import B from "./B";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-[#8b5cf6]">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
