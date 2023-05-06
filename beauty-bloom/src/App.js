375a131600dedf54ee48e6be77af4ca51c30f3c3
majdiShomali <majdi.shomali.1997@gmail.com>
2023-05-06 13:00:00 +0300

    add cart page

--- beauty-bloom/src/App.js
+++ beauty-bloom/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Product from "./pages/Product/product";
+// import Product from "./pages/Product/product";
 import Cart from "./pages/Product/Cart";
 function App() {
   return (
     <div>
-      <Product/>
+      <Cart/>
     </div>
   );
 }
--- beauty-bloom/src/pages/Product/Cart.js
+++ beauty-bloom/src/pages/Product/Cart.js
@@ -1,59 +1,112 @@
-// import React from 'react'
-// import "./Cart.css"
-// import product0001 from "./images/product0001.jpg"
-// const Cart = () => {
-//   return (
-//     <>
-//     <div className='containerCart'>
+import React from 'react'
+import "./Cart.css"
+import product0001 from "./images/product0001.jpg"
+const Cart = () => {
+  return (
+    <>
+    <div className='containerCart'>
 
-//       <div className='CartSection1'>
+      <div className='CartSection1'>
 
-//       <div className='s1MyCart'>
-//         <p>My cart</p>
-//         <p>continue shopping</p>
-//       </div>
-//       <hr/>
+      <div className='s1MyCart'>
+        <p>My cart</p>
+        <p>continue shopping</p>
+      </div>
+      <hr/>
 
-//        <div className='s1MyCartInfo'>
+       <div className='s1MyCartInfo'>
         
-//         <div className='CartProductInfo'>
-//         <img src={product0001} alt=''/>
-//         <div className='CartProductInfoText'>
-//         <p>Luxury Matte Lipstick</p>
-//         <p>$37.00</p>
-//         <p>Shades: Lily</p>
-//         </div>
+        <div className='CartProductInfo'>
+        <img src={product0001} alt=''/>
+        <div className='CartProductInfoText'>
+        <p>Luxury Matte Lipstick</p>
+        <p>$37.00</p>
+        <p>Shades: Lily</p>
+        </div>
         
-//         </div>
+        </div>
 
 
-//         <div>
+        <div className='productCartQuantityC'>
         
-//         <div className='productCartQuantity'>
-//             <button>-</button>
-//              <input type='number'/>
-//             <button>+</button>
-//         </div>
+        <div className='productCartQuantity'>
+            <button style={{width:"2rem"}}>-</button>
+             <input style={{width:"2rem"}} type='number'/>
+            <button style={{width:"2rem"}}>+</button>
+        </div>
 
-//         </div>
+        <p>$37.00</p>
+        <button style={{width:"2rem"}}>X</button>
+
+        </div>
 
 
-//        </div>
+       </div>
+         <hr/>
+
+     
+     <div className='promeNoteC'>
+    
+    <div className='promeNote'>
+        <img/>
+        <p> Enter a promo code</p>
+    </div>
+    <div className='promeNote'>
+        <img/>
+        <p> Add a note</p>
+    </div>
+     
+
+     </div>
 
 
-//       </div>
 
-//       <div className='CartSection2'>
+      </div>
 
-//       </div>
+      <div className='CartSection2'>
 
-//     </div>
+        <p>Order summary</p>
+        <hr/>
+
+       <div className='CartSection2Price'>
+        <p>subtotal</p>
+        <p>$37.00</p>
+       </div> 
+
+
+       <p>Estimate Shipping</p>
+
+         <hr/>
+
+
+         <div className='CartSection2Price'>
+        <p>total</p>
+        <p>$37.00</p>
+       </div> 
+
+        <div className='paymentButtons'>
+          <button className='checkoutButton'>checkout</button>
+          <button className='paypalButton'>paypal   checkout</button>
+        </div>  
+
+
+        <div className='secureCheckout'>
+           <img/>
+           <p>secure checkout </p>
+        </div>
+
+      </div>
+
+    
+
+
+    </div>
     
     
     
-//     </>
+    </>
     
-//   )
-// }
+  )
+}
 
-// export default Cart
+export default Cart
