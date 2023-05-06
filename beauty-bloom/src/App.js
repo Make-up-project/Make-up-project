17e34a40775fd023c2c06d65f871eb0142e62bba
majdiShomali <majdi.shomali.1997@gmail.com>
2023-05-06 13:04:35 +0300

    edit conflict

--- beauty-bloom/src/App.js
+++ beauty-bloom/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./pages/NavAndFooter/Footer";
-import Header from "./pages/NavAndFooter/Nav";
+// import Header from "./pages/NavAndFooter/Nav";
 import Contact from "./pages/landing/ContactUs/Contact";
 import About from "./pages/landing/aboutPage/About";
 import Landing from "./pages/landing/landingPage/Landing";
@@ -11,7 +11,7 @@ import Landing from "./pages/landing/landingPage/Landing";
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route element={<Landing />} path="Landing" />
         <Route element={<Contact />} path="Contact" />
