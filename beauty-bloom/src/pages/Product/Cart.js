6d5e9e249cc78bb8fa17a64be2b6f9b73752d49a
majdiShomali <majdi.shomali.1997@gmail.com>
2023-05-06 11:32:41 +0300

    add product page

---
+++ beauty-bloom/src/pages/Product/Cart.js
@@ -0,0 +1,59 @@
+// import React from 'react'
+// import "./Cart.css"
+// import product0001 from "./images/product0001.jpg"
+// const Cart = () => {
+//   return (
+//     <>
+//     <div className='containerCart'>
+
+//       <div className='CartSection1'>
+
+//       <div className='s1MyCart'>
+//         <p>My cart</p>
+//         <p>continue shopping</p>
+//       </div>
+//       <hr/>
+
+//        <div className='s1MyCartInfo'>
+        
+//         <div className='CartProductInfo'>
+//         <img src={product0001} alt=''/>
+//         <div className='CartProductInfoText'>
+//         <p>Luxury Matte Lipstick</p>
+//         <p>$37.00</p>
+//         <p>Shades: Lily</p>
+//         </div>
+        
+//         </div>
+
+
+//         <div>
+        
+//         <div className='productCartQuantity'>
+//             <button>-</button>
+//              <input type='number'/>
+//             <button>+</button>
+//         </div>
+
+//         </div>
+
+
+//        </div>
+
+
+//       </div>
+
+//       <div className='CartSection2'>
+
+//       </div>
+
+//     </div>
+    
+    
+    
+//     </>
+    
+//   )
+// }
+
+// export default Cart
