96f4676df8b95693cd0f07d70530b01202d3feaf
fares mohalhel <fares.mohalhel@gmail.com>
2023-05-10 20:48:42 +0300

    updated card design to prevent cart btn overlapping with colors

--- beauty-bloom/src/components/StoreCard.jsx
+++ beauty-bloom/src/components/StoreCard.jsx
@@ -71,7 +71,9 @@ const StoreCard = (props) => {
             )}{" "}
           </span>
         </span>
-        <span className="mt-3 flex gap-2 flex-wrap">
+      </div>
+      <div className="flex justify-between gap-3 items-end p-3 justify-self-end mt-auto">
+        <span className="mt-3 flex gap-2 flex-wrap ">
           {props.availableColors.length !== 0 ? (
             props.availableColors.map((color) => {
               return (
@@ -86,26 +88,26 @@ const StoreCard = (props) => {
             <p className="">No available Colors</p>
           )}
         </span>
-      </div>
-      <button
-        className="absolute bottom-5 right-5 w-12 h-12 rounded-full bg-pink-100 cursor-pointer flex items-center justify-center"
-        onClick={clickCartAddHandler}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
+        <button
+          className="flex-shrink-0 w-12 h-12 rounded-full bg-pink-100 cursor-pointer flex items-center justify-center"
+          onClick={clickCartAddHandler}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
